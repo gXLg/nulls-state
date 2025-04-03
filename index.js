8980c80da1c9757aacdf235e47af3cc7c3a8ed07
gXLg <natgaev@gmail.com>
2025-04-03 20:46:40 +0200

    update naming; improve secret key config

--- index.js
+++ index.js
@@ -3,20 +3,21 @@ const { optparser } = require("gxlg-utils");
 
 const parser = optparser([
   { "name": "cookie", "types": ["state"]                   },
-  { "name": "secret", "types": [""],      "required": true },
+  { "name": "jwt",    "types": ["", []],  "required": true },
   { "name": "secure", "types": [true]                      }
 ]);
 
 module.exports = (opt = {}) => {
   const options = parser(opt);
 
+  const jwtSecret = Buffer.from(options.jwt);
   const DURATION = (24 * 60 * 60 * 1000).toString(); // 1 day
 
   const plugin = (req, res) => {
     const cookie = req.cookies[options.cookie];
     let s = {};
     try {
-      const { state } = jwt.verify(cookie, options.secret);
+      const { state } = jwt.verify(cookie, jwtSecret);
       s = state;
     } catch {}
 
@@ -28,7 +29,7 @@ module.exports = (opt = {}) => {
       if (name in s) {
         const v = s[name];
         delete s[name];
-        const token = jwt.sign({ "state": s }, options.secret, { "expiresIn": DURATION });
+        const token = jwt.sign({ "state": s }, jwtSecret, { "expiresIn": DURATION });
         const opt = {
           "httpOnly": true,
           "sameSite": true,
@@ -43,7 +44,7 @@ module.exports = (opt = {}) => {
 
     res.putState = (name, value) => {
       s[name] = value;
-      const token = jwt.sign({ "state": s }, options.secret, { "expiresIn": DURATION });
+      const token = jwt.sign({ "state": s }, jwtSecret, { "expiresIn": DURATION });
       const opt = {
         "httpOnly": true,
         "sameSite": true,
@@ -53,17 +54,17 @@ module.exports = (opt = {}) => {
     };
   };
 
-  return options => {
-    const prev = options.hook;
-    options.hook = async (req, res) => {
+  return noptions => {
+    const prev = noptions.hook;
+    noptions.hook = async (req, res) => {
       plugin(req, res);
       await prev(req, res);
     };
 
-    if ("^" in options.srcProviders) {
+    if ("^" in noptions.srcProviders) {
       console.warn("Can't initialize the shorthand state provider: It overwrites an already existing provider.");
     } else {
-      options.srcProviders["^"] = v => {
+      noptions.srcProviders["^"] = v => {
         return r => r.popState(v) ?? "";
       };
     }
